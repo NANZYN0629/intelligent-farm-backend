e585e797027acf3151b114b5ce23f31c05cc5b30
南瓜 <3282796667@qq.com>
2025-03-18 22:06:20 +0800

    完成了 种植日志 传感器

---
+++ src/main/java/com/sipc/intelligentfarmbackend/controller/EnvironmentController.java
@@ -0,0 +1,31 @@
+package com.sipc.intelligentfarmbackend.controller;
+
+
+import com.sipc.intelligentfarmbackend.pojo.Environment;
+import com.sipc.intelligentfarmbackend.pojo.Result;
+import com.sipc.intelligentfarmbackend.service.EnvironmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/environment")
+@RestController
+public class EnvironmentController {
+
+    @Autowired
+    private EnvironmentService environmentService;
+
+
+    // 根据fieldId 查询环境信息
+    @GetMapping
+    public Result getByFieldId(Integer fieldId) {
+           log.info("查询环境信息，fieldId={}", fieldId);
+        Environment environment = environmentService.getByFieldId(fieldId);
+        return Result.success(environment);
+    }
+
+}
---
+++ src/main/java/com/sipc/intelligentfarmbackend/mapper/EnvironmentMapper.java
@@ -0,0 +1,14 @@
+package com.sipc.intelligentfarmbackend.mapper;
+
+import com.sipc.intelligentfarmbackend.pojo.Environment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface EnvironmentMapper {
+
+
+    // 根据fieldId查询环境信息
+//    @Select("select * from environment where field_id=#{fieldId}")
+    Environment getByFieldId(Integer fieldId);
+}
