7417ab5b1a967429e9feed63f3fa3d66d66d8813
NANZYN0629 <3282796667@qq.com>
2025-04-28 17:18:01 +0800

    进一步完善联桥，配置跨域资源共享 (CORS) 的配置文件，增加警告传输

--- src/main/java/com/sipc/intelligentfarmbackend/config/CorsConfig.java
+++ src/main/java/com/sipc/intelligentfarmbackend/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 允许所有路径
-                .allowedOrigins("http://localhost") // 你的前端地址（注意不要带斜杠）
+                .allowedOrigins("http://localhost:8081") // 你的前端地址（注意不要带斜杠）
                 .allowedMethods("*")    // 允许所有HTTP方法（GET/POST等）
                 .allowedHeaders("*")    // 允许所有请求头
                 .allowCredentials(true)     // 允许携带cookie等凭证
