87fd87a66c2b8920ecfa823d2809cdbcf35e8353
NANZYN0629 <3282796667@qq.com>
2025-05-02 20:06:14 +0800

    完善，地块的mybatis映射文件

--- src/main/java/com/sipc/intelligentfarmbackend/mapper/FieldMapper.java
+++ src/main/java/com/sipc/intelligentfarmbackend/mapper/FieldMapper.java
@@ -9,21 +9,22 @@ import java.util.List;
 public interface FieldMapper {
 
     //查询所有地块
-    @Select("select field.id, field.field_name, field.crop_name, field.crop,field.area,field.plant_time from field")
+//    @Select("select field.id, field.field_name, field.crop_name, field.crop,field.area,field.plant_time from field")
     public List<Field> findAll();
 
-    @Select("select * from field where id=#{id}")
+    //根据id查询地块
+//    @Select("select * from field where id=#{id}")
     List<Field> getById(Integer id);
     //添加地块
-    @Insert("insert into field(field_name,location,area,crop_name,crop,plant_time) values(#{fieldName},#{location},#{area},#{cropName},#{crop},#{plantTime})")
+//    @Insert("insert into field(field_name,location,area,crop_name,crop,plant_time) values(#{fieldName},#{location},#{area},#{cropName},#{crop},#{plantTime})")
     void insert(Field field);
 
     // 根据id删除地块
-    @Delete("delete from field where id=#{id}")
+//    @Delete("delete from field where id=#{id}")
     void deleteById(Integer id);
 
     // 根据id修改地块信息
-    @Update("update field set area=#{area},crop_name=#{cropName},crop=#{crop},plant_time=#{plantTime} where id=#{id}")
+//    @Update("update field set area=#{area},crop_name=#{cropName},crop=#{crop},plant_time=#{plantTime} where id=#{id}")
     void update(Field field);
 
 }
